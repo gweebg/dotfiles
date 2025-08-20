6938f555f2259250bfe0ccdb8680bf264fb954a6
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2025-08-20 22:53:34 +0100

    feat: dnf, terminal + scripts

---
+++ scripts/fonts.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+REPO="git@github.com:ryanoasis/nerd-fonts.git"
+INSTALL_DIR="$HOME/.nerd-fonts"
+SPACE_PER_FONT_MB=300
+
+FONTS=("CascadiaCode" "CodeNewRoman" "DejaVuSansMono" "FiraCode" "FiraMono" "Hack" "Iosevka" "JetBrainsMono" "NerdFontsSymbolsOnly" "Noto" "Ubuntu" "UbuntuMono")12
+
+error() {
+    echo "error: $*" >&2
+    exit 1
+}
+
+check_space() {
+    local required=$(( ${#FONTS[@]} * SPACE_PER_FONT_MB ))
+    local available
+    available=$(df -m "$HOME" | awk 'NR==2 {print $4}') || error "failed to check available disk space"
+
+    echo "required (aprox.): ${required}MB, available: ${available}MB"
+    if (( available < required )); then
+        error "not enough disk space"
+    fi
+}
+
+clone_repo() {
+    if [[ ! -d "$INSTALL_DIR" ]]; then
+        git clone --filter=blob:none --sparse "$REPO" "$INSTALL_DIR" \
+            || error "failed to clone repository"
+    fi
+}
+
+checkout_fonts() {
+    cd "$INSTALL_DIR" || error "failed to enter install directory"
+    git sparse-checkout set install.sh $(printf "patched-fonts/%s " "${FONTS[@]}") \
+        || error "failed to checkout fonts"
+}
+
+install_fonts() {
+    cd "$INSTALL_DIR" || error "failed to enter install directory"
+    ./install.sh || error "failed to install $font"
+}
+
+cleanup() {
+    if [[ -d "$INSTALL_DIR" ]]; then
+        echo "cleaning up install directory..."
+        rm -rf "$INSTALL_DIR" || error "failed to remove install directory"
+    fi
+}
+
+check_space
+clone_repo
+checkout_fonts
+install_fonts
+cleanup
+
+echo "done. installed: ${FONTS[*]}"
---
+++ scripts/pkgs.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+PKGS=(
+    eza
+    zoxide
+    starship
+    fish
+    docker
+    git
+    gitui
+    pre-commit
+    direnv
+    tldr
+    fd-find
+    fzf
+)
+
+error() {
+    echo "error: $*" >&2
+    exit 1
+}
+
+install_dnf_packages() {
+    echo "updating dnf..."
+    sudo dnf -y update || error "dnf update failed"
+
+    echo "installing packages: ${PKGS[*]}"
+    sudo dnf -y install "${PKGS[@]}" || error "failed to install base packages"
+}
+
+enable_services() {
+    echo "enabling docker..."
+    sudo systemctl enable --now docker || error "failed to enable docker"
+    sudo usermod -aG docker $USER || error "failed to add $USER to docker group"
+}
+
+install_dnf_packages
+install_asdf
+enable_services
+
+echo "done. installed: ${PKGS[*]}"
