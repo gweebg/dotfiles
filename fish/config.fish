6a7794a4d91b7b90d04b3c1e3cbc4c93501da279
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2026-01-03 12:24:29 +0000

    feat: clean fish & starship

--- fish/config.fish
+++ fish/config.fish
@@ -5,14 +5,13 @@ end
 bind \e\[3\;5~ kill-word
 
 # Abbreviations
-abbr -a -- rc '$EDITOR /home/guilherme/.config/fish/config.fish'
+abbr -a -- rc 'nvim /home/guilherme/.config/fish/config.fish'
 abbr -a -- open xdg-open
-abbr -a -- penv 'python3 -m venv .env'
 abbr -a -- icat 'kitten icat'
 abbr -a -- g gitui
 abbr -a -- cd 'z'
 abbr -a -- ls 'eza'
-abbr -a -- ll 'eza -alh'
+abbr -a -- ll 'eza -alhg'
 abbr -a -- tree 'eza --tree'
 
 # Key-binds
@@ -24,24 +23,17 @@ abbr -a -- tree 'eza --tree'
 # Alt + s          :  Prepend 'sudo' to command  
 
 
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
-# Run starship
+# starship
 starship init fish | source
+
+# z
 zoxide init fish | source
 
+# direnv
 direnv hook fish | source
 
 # source ~/.asdf/asdf.fish
+/home/guilherme/.local/bin/mise activate fish | source
+
+# opencode
+fish_add_path /home/guilherme/.opencode/bin
